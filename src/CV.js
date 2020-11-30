f14c83e2569f38631f9467658cd32882dc71ecb7
dewi <72603910+dewilestaridee@users.noreply.github.com>
2020-11-30 22:53:36 +0700

    Update CV.js

--- src/CV.js
+++ src/CV.js
@@ -3,6 +3,7 @@ import './Main.css';
 
 import img21 from './cv.png';
 import img22 from './cv1.png';
+import img23 from './cv2.png';
 
 class Cv extends Component {
     render(){
@@ -11,6 +12,7 @@ class Cv extends Component {
             <h3>CV</h3>
             <img class="tengah" top width="40%" height="40%" src={img21} alt="Card cap" /> 
             <img class="tengah" top width="40%" height="40%" src={img22} alt="Card cap" /> 
+            <img class="tengah" top width="40%" height="40%" src={img23} alt="Card cap" /> 
             </div>
         );
     }
