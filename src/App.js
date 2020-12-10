6c7c8afd75bc77580b1698afe3c30d9f4f5f8373
dewi <72603910+dewilestaridee@users.noreply.github.com>
2020-12-10 13:45:47 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Main from './Main.js';
-import CV from './CV.js';
+import Cv from './CV.js';
 import Banner from './Banner.js';
 import Foto from './Foto.js';
 import Certificate from './Certificate.js';
